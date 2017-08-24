64fd1dabfcb7cc2d8ae5d2cf6ba1ed875740c068
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-08-24 00:33:49 -0400

    site displaying data

--- server.rb
+++ server.rb
@@ -1,8 +1,19 @@
+require 'nokogiri'
+require 'httparty'
 require 'sinatra'
 
+page = HTTParty.get('https://miami.craigslist.org/search/mdc/jjj')
+parse_page = Nokogiri::HTML(page)
+jobs_array = []
+x = 1
+parse_page.css('.hdrlnk').each do |item|
+  jobs_array << x.to_s+". "+(item)+"\n"+ "https://miami.craigslist.org" + item.attributes["href"].value
+  x += 1
+end
 
 get '/' do
-  File.read(File.join('public', 'hello.txt'))
+
+  return jobs_array.join
 end
 
 
